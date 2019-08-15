a29503ec1a23ea5494e8c715c0bf645e281321a7
Ryan McCahon <mccahonr@yahoo.com>
2019-08-15 17:02:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+      array.join(" ")
+    elsif array.length == 2
+      array.join(" and ")
+    elsif array.length >= 3
+      new_last_item = "and #{array[-1]}"
+      array.pop
+      array.push(new_last_item)
+      array.join(", ")
+  end
 end
